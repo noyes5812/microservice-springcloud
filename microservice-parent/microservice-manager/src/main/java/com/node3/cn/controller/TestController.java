5781fb00372eded3eaa3f8197152258978e6f9c3
noyes5812 <85227727@qq.com>
2019-09-25 14:49:32 +0800

    提交

--- microservice-parent/microservice-manager/src/main/java/com/node3/cn/controller/TestController.java
+++ microservice-parent/microservice-manager/src/main/java/com/node3/cn/controller/TestController.java
@@ -5,15 +5,15 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class TestController {
 
     @ApiOperation(value="TEST", notes="根据userId获取TEST")
-    @ApiImplicitParam(name = "userId", value = "用户ID", required = true, dataType = "String", paramType = "path")
-    @GetMapping("/test")
-    public String test(String userId){
+    @RequestMapping("/test")
+    public String test(){
         return "test";
     }
 
