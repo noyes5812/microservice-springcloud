c0f888d284d27671e97e026909be33174195fe5a
noyes5812 <85227727@qq.com>
2019-09-25 15:00:15 +0800

    提交

--- microservice-parent/microservice-manager/src/main/java/com/node3/cn/controller/TestController.java
+++ microservice-parent/microservice-manager/src/main/java/com/node3/cn/controller/TestController.java
@@ -6,6 +6,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
@@ -14,7 +15,7 @@ public class TestController {
     @ApiOperation(value="TEST", notes="根据userId获取TEST")
     @RequestMapping("/test")
     public String test(){
-        return "test";
+        return "redirect:/test";
     }
 
 
